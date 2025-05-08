83505462eb7f87e879cab05d7fc4caeae3ccf42a
Prakash Rai <prakashraipersonal@gmail.com>
2025-05-08 10:13:07 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import datetime\import pytz
+import datetime
+import pytz
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from streamlit_autorefresh import st_autorefresh
